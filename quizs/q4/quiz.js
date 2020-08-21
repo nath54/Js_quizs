2e6ec6afc20370aeac6ecca338b5ab4438aaa05b
nath54 <=>
2020-08-21 18:57:58 +0200

    aa

---
+++ quizs/q4/quiz.js
@@ -0,0 +1,27 @@
+
+var q={type:"",question:"",reponses:[],br:0};
+
+/*
+
+EXEMPLES : 
+
+var q1={type:"qcu",question:"Quand nait l'Assemblée Nationale ?",reponses:["le 16 juin 1789","le 17 juin 1789","le 18 juin 1789"],br:"le 17 juin 1789"};
+var q18={type:"dat",question:"Quand Louis XVI meurt-il ?",reponses:[1785,1815],br:["21","janvier","1793"]};
+var q3={type:"inp",question:"Définition de PIB",reponses:[],br:"Somme des richesses produites dans un pays et en une année donnée"};
+*/
+
+var q1={type:"dat",question:"Quand est-ce que la seconde guerre mondiale a commencée ?",reponses:[1914,1983],br:["1","septembre","1939"]};
+var q2={type:"dat",question:"Quand est-ce que la seconde guerre mondiale s'est-elle terminée ?",reponses:[1914,1983],br:["2","septembre","1945"]};
+var q3={type:"qcu",question:"Comment se nomment l'Allemagne et ses alliés lors de la 2nd guerre mondiale ?",reponses:["Forces of the Haches","les forces de l'Axe","Les forts des Axes"],br:"les forces de l'Axe"};
+var q4={type:"qcu",question:"Comment se passe le début de la 2nd guerre mondiale pour l'Allemagne et ses alliés ?",reponses:["Ils soumettent rapidement l'Europe","Ils se font soumettre rapidement par leurs ennemis","Personne ne prend le dessus au début de la guerre"],br:"Ils soumettent rapidement l'Europe"};
+var q5={type:"qcu",question:"Qui poursuit seul la lutte pendant la seconde guerre mondiale lorque ses alliés sont soumis ?",reponses:["L'Allemagne","La France","le Royaume-Uni"],br:"le Royaume-Uni"};
+
+
+
+var questions=[
+	q1,
+	q2,
+	q3,
+	q4,
+	q5
+];
