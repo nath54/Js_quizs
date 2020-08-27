a9d7a9d64243f5884e8f9c53488404b24e0d974d
nath54 <cerisara.nathan@gmail.com>
2020-08-27 11:39:16 +0200

    aa

--- quizs/q4/quiz.js
+++ quizs/q4/quiz.js
@@ -12,7 +12,7 @@ var q3={type:"inp",question:"Définition de PIB",reponses:[],br:"Somme des riche
 
 var q1={type:"dat",question:"Quand est-ce que la seconde guerre mondiale a commencée ?",reponses:[1914,1983],br:["1","septembre","1939"]};
 var q2={type:"dat",question:"Quand est-ce que la seconde guerre mondiale s'est-elle terminée ?",reponses:[1914,1983],br:["2","septembre","1945"]};
-var q3={type:"qcu",question:"Comment se nomment l'Allemagne et ses alliés lors de la 2nd guerre mondiale ?",reponses:["Forces of the Haches","les forces de l'Axe","Les forts des Axes"],br:"les forces de l'Axe"};
+//var q3={type:"qcu",question:"Comment se nomment l'Allemagne et ses alliés lors de la 2nd guerre mondiale ?",reponses:["Forces of the Haches","les forces de l'Axe","Les forts des Axes"],br:"les forces de l'Axe"};
 var q4={type:"qcu",question:"Comment se passe le début de la 2nd guerre mondiale pour l'Allemagne et ses alliés ?",reponses:["Ils soumettent rapidement l'Europe","Ils se font soumettre rapidement par leurs ennemis","Personne ne prend le dessus au début de la guerre"],br:"Ils soumettent rapidement l'Europe"};
 var q5={type:"qcu",question:"Qui poursuit seul la lutte pendant la seconde guerre mondiale lorque ses alliés sont soumis ?",reponses:["L'Allemagne","La France","le Royaume-Uni"],br:"le Royaume-Uni"};
 var q6={type:"dat",question:"Quand est-ce que Hitler attaque l'URSS",reponses:[1914,1983],br:["22","juin","1941"]};
@@ -24,7 +24,7 @@ var q9={type:"dat",question:"Quand est-ce que les Etats-Unis rentrent en guerre 
 var questions=[
 	q1,
 	q2,
-	q3,
+	//q3,
 	q4,
 	q5,
 	q6,
