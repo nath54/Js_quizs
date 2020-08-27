4eeee51013aef1603bacf1e472cc9418a1a3f743
nath54 <cerisara.nathan@gmail.com>
2020-08-27 12:17:35 +0200

    aa

--- quizs/q5/quiz.js
+++ quizs/q5/quiz.js
@@ -15,7 +15,9 @@ var q2={type:"qcu",question:"Que vaut ce calcul ?",image:"quizs/q5/q2.svg",repon
 var q3={type:"qcu",question:"Que vaut i² ?",reponses:["1","-1","2"],br:"-1"};
 var q4={type:"qcu",question:"z est un nombre complexe de la forme a+ib, où a et b sont des réels. Que représente a ?",reponses:["La partie réelle du nombre","La partie imaginaire du nombre"],br:"La partie réelle du nombre"};
 var q5={type:"qcu",question:"z est un nombre complexe de la forme a+ib, où a et b sont des réels. Que représente b ?",reponses:["La partie réelle du nombre","La partie imaginaire du nombre"],br:"La partie imaginaire du nombre"};
-
+var q6={type:"qcu",question:"Si Re(z) = 0, que dit-on de z ?",reponses:["z est un imaginaire pur","z est un réel pur","z est un imaginaire complexe","z est un réel complexe"],br:"z est un imaginaire pur"};
+var q7={type:"inp",question:"L'inverse d'un nombre complexe",reponses:[],br:"L'inverse d'un nombre complexe non nul z est le nombre complexe z' tel que z x z'=1"};
+var q8={type:"qcu",question:"Quel est le nombre conjugué du nombre complexe z=a+ib ?",reponses:["a-ib","-a+ib","-a-ib"],br:"a-ib"};
 
 var questions=[
 	q1,
@@ -23,4 +25,7 @@ var questions=[
 	q3,
 	q4,
 	q5,
+	q6,
+	q7,
+	q8,
 ];
