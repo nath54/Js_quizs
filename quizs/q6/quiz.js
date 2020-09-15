58f8359c0ce1c24152c514030d1bead49377987a
nathan cerisara <cerisara.nathan@gmail.com>
2020-09-15 13:07:52 +0200

    aa

---
+++ quizs/q6/quiz.js
@@ -0,0 +1,18 @@
+
+var q={type:"",question:"",reponses:[],br:0};
+
+/*
+
+EXEMPLES : 
+
+var q1={type:"qcu",question:"Quand nait l'Assemblée Nationale ?",reponses:["le 16 juin 1789","le 17 juin 1789","le 18 juin 1789"],br:"le 17 juin 1789"};
+var q18={type:"dat",question:"Quand Louis XVI meurt-il ?",reponses:[1785,1815],br:["21","janvier","1793"]};
+var q3={type:"inp",question:"Définition de PIB",reponses:[],br:"Somme des richesses produites dans un pays et en une année donnée"};
+*/
+
+var q1={type:"qcu",question:"Que connait l'économie mondiale en 1924",reponses:["une phase de croissance","une phase de décroissance","une phase constante"],br:"une phase de croissance"};
+var q2={type:"qcu",question:"Quel est le pays qui a une phase de prospérité spectaculaire à partir de 1924 ?",reponses:["une phase de croissance","une phase de décroissance","une phase constante"],br:"une phase de croissance"};
+
+var questions=[
+	q1,
+];
